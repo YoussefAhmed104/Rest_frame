6960caff4de3650b1700fcb66b601264f023d973
YoussefAhmed104 <youssifahmed104@gmail.com>
2025-03-22 14:59:04 +0200

    show the all details of one product

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/' , views.hello, name= 'Hello')
+    path('<int:pk>/', views.ProductDetailAPIview.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from rest_framework import generics  # استيراد generics من rest_framework لتوفير واجهات API جاهزة
+from .models import Product  
+from .serializers import ProductSerializer 
 
-# Create your views here.
-
-def hello(request):
-    pass
+class ProductDetailAPIview(generics.RetrieveAPIView):  # تعريف فئة API لعرض تفاصيل منتج واحد
+    queryset = Product.objects.all()  # تحديد جميع كائنات Product كاستعلام أساسي
+    serializer_class = ProductSerializer  # تحديد ProductSerializer كفئة التسلسل المستخدمة
